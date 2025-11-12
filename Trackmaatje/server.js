689b1e74c9053d26ede8e48ff7b9021493a5cfc9
87606 <thomvandervorst@gmail.com>
2025-11-12 15:05:04 +0100

    tailwind

--- Trackmaatje/server.js
+++ Trackmaatje/server.js
@@ -24,4 +24,4 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on localhost:${PORT}`));
