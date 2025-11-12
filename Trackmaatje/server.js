075f8115e3a8af51f1ee31f6fc8e7a0092294567
87606 <thomvandervorst@gmail.com>
2025-11-12 14:15:08 +0100

    initial setup

---
+++ Trackmaatje/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(
+  session({
+    secret: "poep", 
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.render("index",{ title: "home"});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
