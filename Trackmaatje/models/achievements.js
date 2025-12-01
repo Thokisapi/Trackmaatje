098e5661df3031975903bc563d11633dfd395bc9
87606 <thomvandervorst@gmail.com>
2025-12-01 10:28:34 +0100

    achievements model

--- Trackmaatje/models/achievements.js
+++ Trackmaatje/models/achievements.js
@@ -3,8 +3,17 @@ const Schema = mongoose.Schema;
 
 const achievementsSchema = new Schema(
   {
-    
+    achievement:{
+      type: String,
+    },
+    achievementStatus:{
+      type: Boolean,
+      default: false,
+    },
+    dateAchieved:{
+      type: Date,
+    },
   },
-    { timestamps: true },
+  { timestamps: true }
 );
 module.exports = mongoose.model('achievements', achievementsSchema);
